759b552dd183433d6823a531023dfba048b39992
Nastya11 <cat110399@gmail.com>
2013-12-15 13:33:34 +0400

    commit

---
+++ GameName7/GameName7/Screens/BackgroundScreen.cs
@@ -0,0 +1,109 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// BackgroundScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace GameStateManagement
+{
+    /// <summary>
+    /// The background screen sits behind all the other menu screens.
+    /// It draws a background image that remains fixed in place regardless
+    /// of whatever transitions the screens on top of it may be doing.
+    /// </summary>
+    class BackgroundScreen : GameScreen
+    {
+        #region Fields
+
+        ContentManager content;
+        Texture2D backgroundTexture;
+
+        #endregion
+
+        #region Initialization
+
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public BackgroundScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+        }
+
+
+        /// <summary>
+        /// Загружаем графический контент для этого экрана. Текстура заднего фона довольна большая,
+        /// поэтому мы будем использовать наш локальный Content Manager для загрузки его.
+        /// Это позволяет нам разгрузить его перед тем, как перемещаться из меню в саму игру.
+        /// Если мы используем общий ContentManager обеспеченный классом Game, контент останется
+        /// загружаться навсегда.
+
+        /// </summary>
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+            backgroundTexture = content.Load<Texture2D>("background");
+        }
+
+
+        /// <summary>
+        ///Разгружает графический контент для этого экрана.
+        /// </summary>
+        public override void UnloadContent()
+        {
+            content.Unload();
+        }
+
+
+        #endregion
+
+        #region Update and Draw
+
+
+        /// <summary>
+        /// В отличие от других экранов, этот не следует
+        /// пересекать даже если на него накладывается другой экран: он предположительно будет наложен после всех действий!
+        /// Это перезагрузка заставляет параметры coveredByOtherScreen отменить порядок базового метода Update, который исчезает
+        /// </summary>
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                                       bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+        }
+
+
+        /// <summary>
+        /// Отрисовка экрана заднего фона.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            Rectangle fullscreen = new Rectangle(0, 0, viewport.Width, viewport.Height);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(backgroundTexture, fullscreen,
+                             new Color(TransitionAlpha, TransitionAlpha, TransitionAlpha));
+
+            spriteBatch.End();
+        }
+
+
+        #endregion
+    }
+}
---
+++ GameName7/GameName7/Screens/PlayerIndexEventArgs.cs
@@ -0,0 +1,34 @@
+﻿
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+#endregion
+
+namespace GameStateManagement
+{
+    /// <summary>
+    /// Пользовательский аргумент события, которое включает в себя индекс игрока, инициировавшего событие. 
+    /// Это используется в случае MenuEntry.Selected
+    /// </summary>
+    class PlayerIndexEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public PlayerIndexEventArgs(PlayerIndex playerIndex)
+        {
+            this.playerIndex = playerIndex;
+        }
+
+
+        /// <summary>
+        /// Получаем индекс игрока, инициировавшего событие.
+        /// </summary>
+        public PlayerIndex PlayerIndex
+        {
+            get { return playerIndex; }
+        }
+
+        PlayerIndex playerIndex;
+    }
+}
